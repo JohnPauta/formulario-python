e09ed2cb84f133cdd1eadfd98fdc6f3a99cb0ce4
JohnPauta <pautajohn271@gmail.com>
2024-09-24 16:41:43 -0500

    se agrego bootstrap

--- app/views.py
+++ app/views.py
@@ -1,12 +1,63 @@
-# views.py
+import json
+
+import requests
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import ComprobarCedulaForm, RegisterUsuarioForm
 from datetime import datetime
 
+#def index(request):
+#    return render(request, 'registro.html')
 def index(request):
-    return render(request, 'registro.html')
+    if request.method == 'POST':
+        try:
+            json_datos = {'a': 'registrarinscripcion',
+                          'key': 'fd3569c8-f83f-49a9-8612-9227c9b93104',
+                          'cedula': request.POST['estudiante[cedula]'],
+                          'nombres': request.POST['estudiante[nombres]'],
+                          'apellido1': request.POST['estudiante[apellido1]'],
+                          'apellido2': request.POST['estudiante[apellido2]'],
+                          'emailinstitucional': request.POST['estudiante[emailinstitucional]'],
+                          'email': request.POST['estudiante[email]'],
+                          'sexo': request.POST['estudiante[sexo]'],
+                          'fechanacimiento': request.POST['estudiante[fechanacimiento]'],
+                          'telefono_movil': request.POST['estudiante[telefono_movil]'],
+                          'telefono_fijo': request.POST['estudiante[telefono_fijo]'],
+                          'direccion': request.POST['estudiante[direccion]'],
+                          'malla': request.POST['estudiante[malla]'],
+                          'coordinacion': request.POST['estudiante[coordinacion]'],
+                          'sesion': request.POST['estudiante[sesion]'],
+                          'habilitado': request.POST['estudiante[habilitado]'],
+                          }
+            respuesta = requests.post('https://sga.isteps.edu.ec/api', data=json_datos)
+            datosrespuesta = json.loads(respuesta.content)
+            if datosrespuesta['result'] == 'ok':
+                return JsonResponse({'success': True,})
+            return JsonResponse({'success': False, 'message': 'Error en creacion'})
+        except Exception as ex:
+            print(ex)
+            return JsonResponse({'success': False, 'message': 'Excepcion: Error en creacion'})
+
+    else:
+
+        return render(request, 'registro.html')
+
+#def index(request):
+#    if request.method == 'POST':
+#        print('entro')
+#        try:
+#            datos = json.loads(request.body)
+#            respuesta = requests.post('https://sga.isteps.edu.ec/api', json=datos)
+#            datosrespuesta = respuesta.json()
+#            if datosrespuesta.get('result') == 'ok':
+#                return JsonResponse({'success': True})
+#            return JsonResponse({'success': False, 'message': 'error en creacion'})
+#        except Exception as e:
+#            return JsonResponse({'success': False, 'message': str(e)})
+#    else:
+#        return render(request, 'registro.html')
+
 
 def verify_cedula(request):
     if request.method == 'POST':
--- settings.py
+++ settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-!*zs*czemo4wj=gios82zitwjdc$q=b&9#28bfwkcnw9fkeqq-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'corsheaders',
+    # 'corsheaders',
 
 ]
 
@@ -53,11 +53,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
