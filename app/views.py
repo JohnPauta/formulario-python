4c6d726091a1818041a1c269717e01fcf87d1c23
JohnPauta <pautajohn271@gmail.com>
2024-09-24 20:12:54 -0500

    corrección en la respuesta de excepción

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import requests
 from django.shortcuts import render
 from django.http import JsonResponse
 
+
 def index(request):
     if request.method == "POST":
         try:
@@ -25,16 +26,19 @@ def index(request):
                 "sesion": request.POST["estudiante[sesion]"],
                 "habilitado": request.POST["estudiante[habilitado]"],
             }
-            respuesta = requests.post("https://sga.isteps.edu.ec/api", data=json_datos)
+            respuesta = requests.post("https://sga.isteps.edu.ec/api", data=json_datos, timeout=10)
             datosrespuesta = json.loads(respuesta.content)
             if datosrespuesta["result"] == "ok":
                 return JsonResponse({"success": True})
-            return JsonResponse({"success": False, "message": "Error en creacion"})
-        except Exception as ex:
+            return JsonResponse({"success": False, "message": "Error en creacion del estudiante"})
+        except request.exceptions.RequestException as ex:
             print(ex)
-            return JsonResponse({"success": False, "message": "Excepcion: Error en creacion"})
+            return JsonResponse(
+                {"success": False, "message": "Excepcion: Error en creacion del estudiante"}
+            )
     else:
         return render(request, "registro.html")
+
 
 def verify_cedula(request):
     if request.method == "POST":
@@ -42,14 +46,20 @@ def verify_cedula(request):
         # Aquí iría la lógica para verificar la cédula
         return JsonResponse({"success": True, "message": "Cédula verificada"})
 
+
 def submit_data(request):
     if request.method == "POST":
         data = request.POST
         # Aquí iría la lógica para enviar los datos al SGA
-        return JsonResponse({"success": True, "message": "Datos del estudiante enviados al SGA"})
+        return JsonResponse(
+            {"success": True, "message": "Datos del estudiante enviados al SGA"}
+        )
+
 
 def submit_comprobante(request):
     if request.method == "POST":
         data = request.POST
         # Aquí iría la lógica para enviar los datos al SGA
-        return JsonResponse({"success": True, "message": "Datos del comprobante enviados al SGA"})
+        return JsonResponse(
+            {"success": True, "message": "Datos del comprobante enviados al SGA"}
+        )
